d6c964e7e894140bcb1b8433de4261dcac25f4a4
mmashharawi2021-cell <mmashharawi2021@gmail.com>
2026-03-10 11:16:20 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD64_sBihdxOH7pL7EVRPTX8tIZp72gs6E",
+  authDomain: "gaza-survey-dashboard.firebaseapp.com",
+  projectId: "gaza-survey-dashboard",
+  storageBucket: "gaza-survey-dashboard.firebasestorage.app",
+  messagingSenderId: "729286974226",
+  appId: "1:729286974226:web:ccce3a8cd2e2547852f1cb"
+};
